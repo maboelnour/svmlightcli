e49531fbf46cc0c65c3774ecaaf3ac1bc11cfb83
Mohammad Aboelnour <mhm.aboelnour@gmail.com>
2025-09-29 13:40:23 +0300

    Change verbosity to extern

--- ext/svmlightcli/svm_hideo.c
+++ ext/svmlightcli/svm_hideo.c
@@ -31,7 +31,7 @@
 
 /* Common Block Declarations */
 
-long verbosity;
+extern long verbosity;
 
 # define PRIMAL_OPTIMAL      1
 # define DUAL_OPTIMAL        2
@@ -1060,3 +1060,4 @@ double *opt_g,*opt_g0,*alpha;
   }
   return(obj);
 }
+
